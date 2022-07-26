e5e773c31771ba74bc148d129a7dbe3e7a02191c
jinified <jinified@gmail.com>
2022-07-26 08:54:42 +0800

    Update to remove unused message

--- hello-world/hello.js
+++ hello-world/hello.js
@@ -1,12 +1,6 @@
-// Lambda function code
-
 module.exports.handler = async (event) => {
   console.log('Event: ', event);
   const responseMessage = `Time is ${Math.floor(Date.now() / 1000)}`
-
-  if (event.queryStringParameters && event.queryStringParameters['Name']) {
-    responseMessage = 'Hello, ' + event.queryStringParameters['Name'] + '!';
-  }
 
   return {
     statusCode: 200,
